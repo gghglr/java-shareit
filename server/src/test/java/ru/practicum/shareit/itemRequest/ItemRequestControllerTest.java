2e5d28da78b35aea7999744d124986f24cf537a7
Alexey <aleshka-yurlov@yandex.ru>
2024-03-11 13:02:00 +0300

    fix codestyle

--- server/src/test/java/ru/practicum/shareit/itemRequest/ItemRequestControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/itemRequest/ItemRequestControllerTest.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.itemRequest;
 
-
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- server/src/test/java/ru/practicum/shareit/user/UserServiceTest.java
+++ server/src/test/java/ru/practicum/shareit/user/UserServiceTest.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.user;
 
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
