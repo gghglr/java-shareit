6b70ae4ffbd9ce03482b4bc9de2c5dd564260148
Alexey <aleshka-yurlov@yandex.ru>
2024-03-11 12:58:19 +0300

    add test

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
@@ -20,8 +20,7 @@ public class BookingClient extends BaseClient {
     @Autowired
     public BookingClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
-                builder
-                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
+                builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
                         .requestFactory(HttpComponentsClientHttpRequestFactory::new)
                         .build()
         );
---
+++ server/src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager em;
+    @Autowired
+    private UserRepository userRepository;
+    User user = new User();
+
+    @BeforeEach
+    void setUp() {
+        user.setEmail("test@mail.ru");
+        user.setName("test");
+        em.persist(user);
+    }
+}
